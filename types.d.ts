6fa28a3794038a980e98f3bc6db8a61b01ceabc6
saadb-asad <saadbasad18@gmail.com>
2025-12-30 16:00:40 +0500

    Fix: Update types.d.ts to module and include in tsconfig

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,19 @@
+export { };
+
 declare global {
     namespace JSX {
         interface IntrinsicElements {
-            'model-viewer': any;
+            'model-viewer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+                src?: string;
+                poster?: string;
+                'ios-src'?: string;
+                alt?: string;
+                'camera-controls'?: boolean;
+                'auto-rotate'?: boolean;
+                ar?: boolean;
+                'ar-modes'?: string;
+                ref?: any;
+            };
         }
     }
 }
